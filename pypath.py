5396574549ded86c8e7c06bbff152facf4efa51c
Shokry <shokry92@gmail.com>
2016-11-27 23:19:25 +0200

    Dummy file write support

--- pypath.py
+++ pypath.py
@@ -5,13 +5,28 @@ from sys import argv
 def main(args):
     # If no directory is specified, use current directory
     dirPath = '.'
-    if (len(args) > 1):
+    if len(args) > 1:
         dirPath = args[1]
-    print ('\n'.join(createPath(dirPath)))
+
+    outFile = None
+    if len(args) > 2:
+        outFile = open(args[2], 'w')
+
+    print ('\n'.join(createPath(dirPath, outFile)))
 
 
-def createPath(dirPath='.', levelAligner='|', fileIndicator='|-',
-               level=0, pathMap=None):
+def createPath(dirPath='.', outFile=None, levelAligner='|',
+               fileIndicator='|-'):
+    path = generatePath(dirPath, levelAligner, fileIndicator)
+
+    if outFile is not None and not outFile.closed:
+        outFile.write('\n'.join(path))
+        outFile.close()
+    return path
+
+
+def generatePath(dirPath, levelAligner,
+                 fileIndicator, level=0, pathMap=None):
     if pathMap is None:
         pathMap = []
     files = []
@@ -26,8 +41,8 @@ def createPath(dirPath='.', levelAligner='|', fileIndicator='|-',
         levelLine = formatLine(levelAligner, fileIndicator, file, level)
         pathMap.append(levelLine)
 
-        createPath(path.join(dirPath, file), levelAligner, fileIndicator,
-                   level+1, pathMap)
+        generatePath(path.join(dirPath, file), levelAligner,
+                     fileIndicator, level+1, pathMap)
 
     return pathMap
 
