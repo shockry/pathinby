b40e1e6805d44208100080c34c5173b65b3859ba
Shokry <shokry92@gmail.com>
2016-11-27 21:38:04 +0200

    Some best practices and Python 3 support

--- pypath.py
+++ pypath.py
@@ -1,28 +1,32 @@
 from os import listdir, path
 from sys import argv
 
-def main(argv):
-    #If no directory is specified, use current directory
-    dir = '.'
-    if (len(argv) > 1):
-        dir = argv[1]
-    print '\n'.join(createPath())
 
-def createPath(dir='.', levelAligner='|', fileIndicator='|-',
-        level=0, pathMap=[]):
+def main(args):
+    # If no directory is specified, use current directory
+    dirPath = '.'
+    if (len(args) > 1):
+        dirPath = args[1]
+    print ('\n'.join(createPath(dirPath)))
+
+
+def createPath(dirPath='.', levelAligner='|', fileIndicator='|-',
+               level=0, pathMap=None):
+    if pathMap is None:
+        pathMap = []
     files = []
-    #If directory, get visit its contents, otherwise just return
+    # If directory, get visit its contents, otherwise just return
     try:
-        files = listdir(dir.strip())
+        files = listdir(dirPath.strip())
     except OSError:
         return
 
     for file in files:
         pathMap.append((levelAligner if level > 0 else "") +
-            (" "*level)+fileIndicator+file)
-        
-        createPath(path.join(dir, file), levelAligner, fileIndicator,
-            level+1, pathMap)
+                       (" "*level)+fileIndicator+file)
+
+        createPath(path.join(dirPath, file), levelAligner, fileIndicator,
+                   level+1, pathMap)
 
     return pathMap
 
